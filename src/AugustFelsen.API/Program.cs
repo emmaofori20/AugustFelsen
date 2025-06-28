756abcc46edf81ec6845ffbe9ee61baa5050fff1
Emmanuel Ofori <emmanuelofori2638@gmail.com>
2025-06-28 08:59:20 +0000

    Add AuthService and simplify Swagger configuration
    - Registered AuthService as a scoped service in Program.cs.
    - Streamlined Swagger setup by removing development checks.
    - Updated CORS configuration comment to reflect policy change.

--- src/AugustFelsen.API/Program.cs
+++ src/AugustFelsen.API/Program.cs
@@ -109,14 +109,20 @@ builder.Services.AddScoped<IdentityService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(c =>
+//    {
+//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "August Felsen API V1");
+//    });
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "August Felsen API V1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "August Felsen API V1");
+});
 
 app.UseHttpsRedirection();
 
